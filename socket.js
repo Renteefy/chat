8ee0655ee96fe3e575575e517ad55437613e05ce
yajatvishwak <yajat472001@gmail.com>
2021-04-16 20:42:37 +0530

    added username detection

--- socket.js
+++ socket.js
@@ -11,43 +11,47 @@ function makeServer(server) {
   wss.on("connection", function connection(ws, req) {
     console.log("A connection has arrived. ");
     ws.id = i++;
-    const username = req.url.split("?")[1].split("=")[1];
-    console.log(username);
-    lookup[ws.id] = ws;
-    redis.set(username, ws.id);
-    ws.on("message", function incoming(message) {
-      console.log("received: %s", message);
+    try {
+      const username = req.url.split("?")[1].split("=")[1];
+      console.log(username);
+      lookup[ws.id] = ws;
+      redis.set(username, ws.id);
+      ws.on("message", function incoming(message) {
+        console.log("received: %s", message);
 
-      try {
-        var json = JSON.parse(message);
-        switch (json.action) {
-          case "privateMessage":
-            storeMessage(json);
-            // saved message to db, now pushing to other receiver client
-            redis.get(json.receiver, function (err, receiverWSID) {
-              if (err) {
-                console.error(err);
-              } else {
-                lookup[receiverWSID].send(
-                  JSON.stringify({
-                    sender: json.sender,
-                    receiver: json.receiver,
-                    message: json.message,
-                    chatID: json.chatID,
-                    time: Date.now().toString(),
-                  })
-                ); // Promise resolves to "bar"
-              }
-            });
-            break;
-          default:
-            break;
+        try {
+          var json = JSON.parse(message);
+          switch (json.action) {
+            case "privateMessage":
+              storeMessage(json);
+              // saved message to db, now pushing to other receiver client
+              redis.get(json.receiver, function (err, receiverWSID) {
+                if (err) {
+                  console.error(err);
+                } else {
+                  lookup[receiverWSID].send(
+                    JSON.stringify({
+                      sender: json.sender,
+                      receiver: json.receiver,
+                      message: json.message,
+                      chatID: json.chatID,
+                      time: Date.now().toString(),
+                    })
+                  ); // Promise resolves to "bar"
+                }
+              });
+              break;
+            default:
+              break;
+          }
+        } catch (err) {
+          console.log(err);
+          //console.error("Error parsing json: Bruhhhhhh what is that json");
         }
-      } catch (err) {
-        console.log(err);
-        //console.error("Error parsing json: Bruhhhhhh what is that json");
-      }
-    });
+      });
+    } catch (err) {
+      console.log(err);
+    }
   });
 }
 
